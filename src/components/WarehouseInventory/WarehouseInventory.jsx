1e5969750adfeb96b127bd75ccaa82a645e1ef30
Laura Courtney <lauraelainecourtney@gmail.com>
2025-03-27 15:17:32 -0300

    Inventory categories fixed. Item layout still needs tweaking

--- src/components/WarehouseInventory/WarehouseInventory.jsx
+++ src/components/WarehouseInventory/WarehouseInventory.jsx
@@ -3,7 +3,6 @@ import { useState, useEffect } from "react";
 import axios from "axios";
 import { Link } from "react-router-dom";
 import Chevron from "../../assets/imgs/chevron_right.svg";
-import EditIconBlue from "../../assets/imgs/edit-blue.svg";
 import { formatStatus } from "../../../utils";
 // import InventoryModal from "../InventoryModal/InventoryModal";
 
@@ -78,7 +77,7 @@ export default function WarehouseInventoryList({ warehouseId }) {
   return (
     <section className="wareouse-inventory">
       <div className="warehouse-inventory__component">
-        <div className="warehouse-inventory__categories">
+        <div className="categories">
           <h4
             className="categories__item"
             onClick={() => handleSort("item_name")}
@@ -181,25 +180,33 @@ export default function WarehouseInventoryList({ warehouseId }) {
             </div>
 
             <div className="inv-icons">
-              <Link
-                to={`/inventory/edit/${item.id}`}
-                className="inv-icons__button inv-icons__edit"
+              <svg
+                viewBox="0 0 24 24"
+                xmlns="http://www.w3.org/2000/svg"
+                onClick={() => openDeleteModal(item)}
+                className="inv-icons__delete"
               >
-                <img src={EditIconBlue} alt="Edit" />
-              </Link>
+                <path
+                  d="M6 19C6 20.1 6.9 21 8 21H16C17.1 21 18 20.1 18 19V7H6V19ZM8 9H16V19H8V9ZM15.5 4L14.5 3H9.5L8.5 4H5V6H19V4H15.5Z"
+                  fill="#C94515"
+                />
+              </svg>
+
               <Link
                 to={`/inventory/edit/${item.id}`}
                 className="inv-icons__button inv-icons__edit"
               >
                 <svg
+                  width="24"
+                  height="24"
                   viewBox="0 0 24 24"
+                  fill="none"
                   xmlns="http://www.w3.org/2000/svg"
-                  className="inv-icons__button inv-icons__delete"
-                  onClick={() => openDeleteModal(item)}
+                  className="inv-icons__right"
                 >
                   <path
-                    d="M6 19C6 20.1 6.9 21 8 21H16C17.1 21 18 20.1 18 19V7H6V19ZM8 9H16V19H8V9ZM15.5 4L14.5 3H9.5L8.5 4H5V6H19V4H15.5Z"
-                    fill="#C94515"
+                    d="M3 17.25V21H6.75L17.81 9.94L14.06 6.19L3 17.25ZM20.71 7.04C21.1 6.65 21.1 6.02 20.71 5.63L18.37 3.29C17.98 2.9 17.35 2.9 16.96 3.29L15.13 5.12L18.88 8.87L20.71 7.04V7.04Z"
+                    fill="#2E66E6"
                   />
                 </svg>
               </Link>
